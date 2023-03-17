82997ef8f74cb85de8c8cdc01d9592eead1081a1
William Lety <william.lety@gmail.com>
2023-03-17 15:54:20 +0100

    Initial commit

--- src/Config/Client/FacebookClientFactory.php
+++ src/Config/Client/FacebookClientFactory.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/*
+OAuth2 Client Bundle
+Copyright (c) KnpUniversity <http://knpuniversity.com/>
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+*/
+
 namespace KnpU\OAuth2ClientBundle\Config\Client;
 
 use GuzzleHttp\Psr7\HttpFactory;

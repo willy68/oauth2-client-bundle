4ff47bdf682eb3a3c2aedd1a318a8bbadc5cc37f
William Lety <william.lety@gmail.com>
2023-03-17 16:06:57 +0100

    Initial commit

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -13,15 +13,6 @@ return (new \PhpCsFixer\Config())
         '@PSR12' => true,
         '@PSR12:risky' => true,
         'yoda_style' => false,
-        'header_comment' => [
-            'header' => <<<EOF
-OAuth2 Client Bundle
-Copyright (c) KnpUniversity <http://knpuniversity.com/>
-
-For the full copyright and license information, please view the LICENSE
-file that was distributed with this source code.
-EOF
-        ]
     ))
     ->setRiskyAllowed(true)
     ->setFinder($finder)
